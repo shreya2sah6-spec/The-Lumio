99b4cfbed52358058b065a7725c0f2055da039cc
shreya <shreya2sah6@gmail.com>
2026-06-16 20:57:20 +0530

    Update Clarity

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
 
-import Clarity from "@microsoft/clarity";
-
-Clarity.init("x7zgwy6p0s");
-
 createRoot(document.getElementById("root")!).render(
   <div className="max-w-[430px] mx-auto w-full min-h-[100dvh] overflow-x-hidden">
     <App />
